b102f727f735b0423c301c1c774c5e4dd45a7fb6
Yogesh Anup Shirke <yshirke005@gmail.com>
2026-07-02 15:27:03 +0530

    render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://https://codeb-internship-backend.onrender.com',
         changeOrigin: true,
         secure: false
       }
